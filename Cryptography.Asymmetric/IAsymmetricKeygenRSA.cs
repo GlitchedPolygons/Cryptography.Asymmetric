33bcae8c736a309425e896c35a2de1b41d259362
Glitched Polygons <glitchedpolygons@protonmail.com>
2019-08-04 20:04:25 +0200

    Implemented AsymmetricCryptographyRSA + unit tests.

---
+++ Cryptography.Asymmetric/IAsymmetricKeygenRSA.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace GlitchedPolygons.Services.Cryptography.Asymmetric
+{
+    /// <summary>
+    /// Asymmetric RSA key generator.
+    /// </summary>
+    public interface IAsymmetricKeygenRSA
+    {
+        /// <summary>
+        /// Generates a new RSA key pair <see cref="Tuple"/>.
+        /// </summary>
+        /// <returns>The key pair <see cref="Tuple"/>, where the first item is the public RSA key and the second is the private key (both PEM-formatted).</returns>
+        Task<Tuple<string, string>> GenerateKeyPair();
+    }
+}
